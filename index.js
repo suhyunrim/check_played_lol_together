b0cd3fd6e88add219cecbb47f9e8237d0cd2cb3c
ZeroBoom <rsh921015@naver.com>
2022-09-26 03:40:20 +0900

    api 최산화 및 개선

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ String.prototype.replaceAll = function(org, dest) {
   return this.split(org).join(dest);
 }
 
-const urlPrefix = "https://acs.leagueoflegends.com/v1/";
-
 const requestLimitCount = 30;
 
 axiosRetry(axios, { retries: 3 });
@@ -26,7 +24,7 @@ const targetDate = new Date(year, month - 1, day);
 if (new Date() - targetDate >= 86400 * 90 * 1000)
   throw new Error('현재로부터 3개월을 초과하는 인자는 넣을 수 없습니다.');
 
-const accountIdDic = {};
+const infoDic = {};
 const scoreDic = {};
 
 function ConvertSimplifiedName(nickname) {
@@ -46,7 +44,7 @@ async function main() {
       const nickname = parsed[0];
       const score = parsed.length > 1 ? Number(parsed[1]) : 1;
       const simplifiedName = ConvertSimplifiedName(nickname);
-      accountIdDic[simplifiedName] = await GetAccountId_V1(nickname);
+      infoDic[simplifiedName] = await GetSummonerInfo(nickname);
       scoreDic[simplifiedName] = score;
     }
 
@@ -55,7 +53,7 @@ async function main() {
       const nickname = parsed[0];
       const simpplifiedName = ConvertSimplifiedName(nickname);
       console.log(`nickname ${nickname}`);
-      if (!await GetMatchListUntil_V1(nickname, targetDate.getTime())) {
+      if (!await GetMatchListUntil(nickname, targetDate.getTime())) {
         resultCache[simpplifiedName] = 0;
       }
     }
@@ -100,11 +98,12 @@ main();
 async function GetCache(cacheKey)
 {
   try {
-    const text = await fs.readFile(`./cache/${cacheKey}`);
-    if (text == '')
+    const uintArray = await fs.readFile(`./cache/${cacheKey}`);
+    if (!uintArray)
       return;
   
-    return text;  
+    const str = new TextDecoder().decode(uintArray);
+    return JSON.parse(str);
   } catch (e) {
     return;
   }
@@ -115,81 +114,84 @@ async function SetCache(cacheKey, text)
   await fs.writeFile(`./cache/${cacheKey}`, String(text));
 }
 
-async function GetAccountId_V1(nickname)
+async function GetSummonerInfo(nickname)
 {
   if (nickname == '')
     return 'nickname is empty';
 
-  const cacheKey = `accountid_${nickname}`;
-  let accountId = await GetCache(cacheKey);
-  if (!accountId || accountId == '')
+  const cacheKey = `summonerInfo_${nickname}`;
+  let info = await GetCache(cacheKey);
+  if (!info || info == '')
   {
-    const result = await Request('players/', {
-      name: nickname,
-      region: 'KR'
-    });
+    info = await Request(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nickname}`);
+    if (info.puuid == undefined && info.response.status != 200)
+      throw new Error(`${nickname}`);
 
-    accountId = result["accountId"];
-    if (!accountId)
-      throw new Error(`${nickname}은 없는 닉네임입니다.`);
-
-    await SetCache(cacheKey, accountId);
+    await SetCache(cacheKey, JSON.stringify(info));
   }
 
-  return accountId;
+  return info;
 }
 
-function GetTeamId(matchData, accountId)
+function GetTeamId(matchData, puuid)
 {
   try {
-    const participantId = matchData.participantIdentities.find(elem => elem.player.accountId == accountId).participantId;
-    return matchData.participants.find(elem => elem.participantId == participantId).teamId;
+    const participantInfo = matchData.info.participants.find(elem => elem.puuid == puuid);
+    return participantInfo.teamId;
   } catch (e) {
-    console.log(`${accountId}은 없는 account id 입니다.`);
+    console.log(`puuid:${puuid} Error - ${e}`);
     return null;
   }
 }
 
-async function GetMatchListUntil_V1(nickname, until)
+async function GetMatchListUntil(nickname, until)
 {
   let tryCount = 0;
   let beginIndex = 0;
-  let endIndex = 10;
   const simpplifiedName = ConvertSimplifiedName(nickname);
 
   while (tryCount++ < requestLimitCount) {
-    const matchList = await GetMatchList_V1(nickname, beginIndex, endIndex);
+    const matchIds = await GetMatchList(nickname, beginIndex, beginIndex + 100);
     sleep(1000);
     
-    const games = matchList.games.games;
-    if (games.length == 0)
+    if (matchIds.length == 0)
       break;
 
-    for (let i = games.length - 1; i >= 0; --i)
+    for (let i = 0; i < matchIds.length; ++i)
     {
-      const game = games[i];
-      const matchData = await GetMatchData_V1(game.gameId);
-      sleep(1000);
+      const matchId = matchIds[i];
+      let matchData = await GetCache(`gameid_${matchId}`);
+      if (!matchData) {
+        matchData = await GetMatchData(matchId);
+        sleep(1000);
+      }
 
       if (!matchData || matchData.gameType == 'CUSTOM_GAME')
         continue;
-        
+
       try
       {
-        if (matchData.gameCreation < until) {
+        if (matchData.info == undefined) {
+          console.log('maybe rate limit exceeded');
+          i--;
+          sleep(15000);
+          continue;
+        }
+
+        if (matchData.info.gameCreation < until) {
           tryCount = requestLimitCount;
           break;
         }
 
-        const playerTeamId = GetTeamId(matchData, accountIdDic[simpplifiedName]);
+        const playerTeamId = GetTeamId(matchData, infoDic[simpplifiedName].puuid);
 
-        for (let participant of matchData.participantIdentities)
+        for (let participant of matchData.info.participants)
         {
-          const summonerName = participant.player.summonerName;
-          const targetSimplifiedName = ConvertSimplifiedName(participant.player.summonerName);
-          if (targetSimplifiedName != simpplifiedName && accountIdDic[targetSimplifiedName])
+          const summonerName = participant.summonerName;
+          const targetSimplifiedName = ConvertSimplifiedName(participant.summonerName);
+          if (targetSimplifiedName != simpplifiedName && infoDic[targetSimplifiedName])
           {
-            const teamId = GetTeamId(matchData, accountIdDic[targetSimplifiedName]);
+            const teamId = GetTeamId(matchData, infoDic[targetSimplifiedName].puuid);
             if (!teamId) {
               tryCount = requestLimitCount;
               break;
@@ -213,18 +215,17 @@ async function GetMatchListUntil_V1(nickname, until)
       catch (e)
       {
         console.log(e);
-        return `${game.gameId} ${JSON.stringify(matchData)}`;
+        return `${matchId} ${JSON.stringify(matchData)}`;
       }
     }
 
-    beginIndex += 10;
-    endIndex += 10;
+    beginIndex += 100;
   }
 
   return !!resultCache[simpplifiedName];
 }
 
-async function GetMatchData_V1(matchId)
+async function GetMatchData(matchId)
 {
   if (matchId == '')
     return;
@@ -233,38 +234,31 @@ async function GetMatchData_V1(matchId)
   let matchData = await GetCache(cacheKey);
   if (!matchData || matchData == '')
   {
-    matchData = await Request(`stats/game/KR/${matchId}`);
+    matchData = await Request(`https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`);
     await SetCache(cacheKey, JSON.stringify(matchData));
-  }
-  else
-  {
-    matchData = JSON.parse(matchData);
   }
 
   return matchData;
 }
 
-async function GetMatchList_V1(nickname, begIndex, endIndex)
+async function GetMatchList(nickname, beginIndex)
 {
   if (nickname == '')
     return "nickname is empty";
 
-  const accountId = await GetAccountId_V1(nickname);
-  const result = await Request('stats/player_history/KR/' + accountId, {
-    begIndex,
-    endIndex
-  });
+  const info = await GetSummonerInfo(nickname);
+  const result = await Request(`https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${info.puuid}/ids`, { start: beginIndex, count: 100 });
   return result;
 }
 
 async function Request(url, params)
 {
-  let requestUrl = urlPrefix + url;
+  let requestUrl = url;
   try {
-    const result = await axios.get(requestUrl, { 
+    const result = await axios.get(encodeURI(requestUrl), { 
       params,
       headers: {
-        'cookie': 'id_token=' + process.env.id_token
+        'X-Riot-Token': process.env.api_key
       }
     });
 
